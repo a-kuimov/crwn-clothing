1d27d34b73ce35b535f41cff43acdb526184e657
a-kuimov <a.kuimov.inbox@gmail.com>
2021-10-15 18:02:19 +0700

    added routing into shop page

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux"
+
+import "./collections-overview.styles.scss"
+
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
+
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+
+
+const CollectionOverview = ({ collections }) => {
+	return (
+		<div className="collection-overview">
+			{collections.map(({ id, ...otherCollectionProps }) => (
+				<CollectionPreview key={id} {...otherCollectionProps} />
+			))}
+		</div>
+	)
+}
+
+const mapStateToProps = createStructuredSelector({
+	collections: selectShopCollections
+})
+
+export default connect(mapStateToProps)(CollectionOverview);
---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./collection.styles.scss";
+
+const CollectionPage = () => (
+	<div className="collection-page">
+		Collection Page
+	</div>
+)
+
+export default CollectionPage;
